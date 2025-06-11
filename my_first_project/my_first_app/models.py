fd8a12e8694535ad59fa6e0ef62feb6ded5c299e
jorge chipana <jorchistauro@gmail.com>
2025-06-11 17:42:53 -0400

    Lista de Autos

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Carro(models.Model):
-    title=models.TextField(max_length=255)
- 
+    title = models.TextField(max_length=255)
+
+
--- my_first_project/my_first_app/views.py
+++ my_first_project/my_first_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def myView(request):
     car_list = [{'title': 'BMW'}, {'title': 'Mazda'}]
     context = {'car_list': car_list}
-    return render(request, 'my_first_app/templates_app/car_list.html', context)
+    return render(request, 'my_first_app/car_list.html', context)
